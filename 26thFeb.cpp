b1a45066c8bac505d16f560b83a2dbedc1b917a4
Simran. <simrankchoubey@gmail.com>
2026-02-26 23:49:34 +0530

    Add solution for counting steps to reduce binary number
    Implement a solution to count steps to reduce a binary number to one.

---
+++ 26thFeb.cpp
@@ -0,0 +1,67 @@
+/*1404. Number of Steps to Reduce a Number in Binary Representation to One
+Given the binary representation of an integer as a string s, return the number of steps to reduce it to 1 under the following rules:
+If the current number is even, you have to divide it by 2.
+If the current number is odd, you have to add 1 to it.
+It is guaranteed that you can always reach one for all test cases.
+Example 1:
+Input: s = "1101"
+Output: 6
+Explanation: "1101" corressponds to number 13 in their decimal representation.
+Step 1) 13 is odd, add 1 and obtain 14. 
+Step 2) 14 is even, divide by 2 and obtain 7.
+Step 3) 7 is odd, add 1 and obtain 8.
+Step 4) 8 is even, divide by 2 and obtain 4.  
+Step 5) 4 is even, divide by 2 and obtain 2. 
+Step 6) 2 is even, divide by 2 and obtain 1.  
+Example 2:
+Input: s = "10"
+Output: 1
+Explanation: "10" corresponds to number 2 in their decimal representation.
+Step 1) 2 is even, divide by 2 and obtain 1.  */
+
+class Solution {
+public:
+    void add(string &s){
+        reverse(s.begin(),s.end());
+        int carry = 1;
+        int i = 0;
+        cout<<"orig:"<<s<<endl;
+        while(carry!=0 && i<s.length()){
+            if(s[i]=='1' && carry==1){
+                s[i] = '0';
+                carry = 1;
+            }
+            else if(s[i]=='0' && carry == 0){
+                s[i] = '0';
+                carry = 0;
+            }
+            else{
+                s[i] = '1';
+                carry = 0;
+            }
+            i++;
+        }
+        if(carry){
+            s.push_back(carry+'0');
+        }
+        reverse(s.begin(),s.end());
+        // cout<<s<<endl;
+        return;
+    }
+    int numSteps(string s) {
+        int count = 0;
+        while(s.length()!=1){
+            int n = s.length();
+            // cout<<s<<endl;
+            if(s[n-1]=='1'){
+                // cout<<"Here"<<endl;
+                add(s);
+            }
+            else{
+                s.pop_back();
+            }
+            count++;
+        }
+        return count;
+    }
+};
